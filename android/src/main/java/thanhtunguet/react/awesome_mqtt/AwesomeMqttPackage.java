3c427ca34fec735a0cacac6738a7f8125e15f1c4
Thanh Tùng <ht@thanhtunguet.info>
2021-04-07 16:15:04 +0700

    chore: add android

---
+++ android/src/main/java/thanhtunguet/react/awesome_mqtt/AwesomeMqttPackage.java
@@ -0,0 +1,27 @@
+package thanhtunguet.react.awesome_mqtt;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class AwesomeMqttPackage implements ReactPackage {
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+
+    List<NativeModule> modules = new ArrayList<>();
+
+    modules.add(new AwesomeMqttModule(reactContext));
+
+    return modules;
+  }
+
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    return Collections.emptyList();
+  }
+}
